dc2b3a3ecd7ff3e06348bd12406fca81a99fe59a
eliasniepoetter <eliasniepoetter@gmail.com>
2023-12-19 14:21:07 +0100

    implemented 3DOF OCP and MCP

--- MPC_3DOF.m
+++ MPC_3DOF.m
@@ -7,12 +7,11 @@ global m Iyy;
 m = 200;
 Iyy = 1000;
 
-% dynamics
-u_dot = @(A_xe, q, w, u) A_xe - q*w;
+% dynamics of the 3-DOF point mass
+u_dot = @(A_xe, q, w) A_xe - q*w;
 w_dot = @(A_ze, q, u) A_ze + q*u;
 
-
-f_A_xe = @(Fx, theta, u) Fx/m - 9.81*sin(theta) - (0.05*0.7*1.225*u^2*0.5)/m;
+f_A_xe = @(Fx, theta, u) Fx/m - 9.81*sin(theta) - 0.05*0.7*1.225*u^2*0.5/200;
 f_A_ze = @(Fz, theta) Fz/m + 9.81*cos(theta);
 
 Xe_dot = @(u, w, theta) u*cos(theta) + w*sin(theta);
@@ -22,65 +21,107 @@ q_dot = @(My) My/Iyy;
 theta_dot = @(q) q;
 
 
-%% Example maneuvre
-
 t0 = 0;
-tf = 60;
-tstep = 0.01;
+tf = 5;
+tstep = 0.1;
 time = t0:tstep:tf;
+
+
+Fx = ones(length(time)-1, 1)*50000;
+Fz = zeros(length(time)-1, 1);
+My = zeros(length(time)-1, 1);
 
 u0 = 300;
 w0 = 0;
 theta0 = 0;
-q0 = 0;
+
+theta  = zeros(length(time), 1);
+theta(1) = theta0;
 
 u  = zeros(length(time), 1);
 u(1) = u0;
 
+
+
+q0 = 0;
+axe0 = 0;
+% axe0 = Fx(1)/m - 9.81*sin(theta(1)) - 0.05*0.7*1.225*u(1)^2*0.5;
+aze0 = 0;
+xe0 = 0;
+ze0 = 0;
+
+
+
 w  = zeros(length(time), 1);
 w(1) = w0;
-
-theta  = zeros(length(time), 1);
-theta(1) = theta0;
 
 q  = zeros(length(time), 1);
 q(1) = q0;
 
 A_xe = zeros(length(time), 1);
-A_xe(1) = 0;
+A_xe(1) = axe0;
 
 A_ze = zeros(length(time), 1);
-A_ze(1) = 0;
+A_ze(1) = aze0;
 
 Xe = zeros(length(time), 1);
-Xe(1) = 0;
+Xe(1) = xe0;
 
 Ze = zeros(length(time), 1);
-Ze(1) = 0;
+Ze(1) = ze0;
 
+
+
+
+
+% output to console
+fprintf('-------------------------------------------------\n');
+fprintf('-- NMPC Ietartion for a missile-target problem --\n');
+fprintf('-------------------------------------------------\n\n');
 
 for i = 1 : length(time)-1
-    % declare control input
-    % if i > 100 && i < 200
-    %     My = 10;
-    % elseif i > 200 && i < 300
-    %     My = -10;
-    % else
-    %     My = 0;
+    % calculate control input
+    X = [u(i); w(i); A_xe(i); A_ze(i); Xe(i); Ze(i); q(i); theta(i)];
+    Tx = 1000;
+    Tz = -200;
+    % [Fx(i), Fz(i), My(i), flags(i)] = OCP_3DOF(m, Iyy, X, [Tx; Tz]);
+    [Fz(i), My(i), flags(i)] = OCP_3DOF(m, Iyy, X, [Tx; Tz]);
+    
+    % if isnan(Fx(i))
+    %     Fx(i) = 0;
     % end
-    My = 0;
-    Fx = 50000;
-    Fz = -1.0*(m*9.81);
+
+    if isnan(Fz(i))
+        Fz(i) = 0;
+    end
+
+    if isnan(My(i))
+        My(i) = 0;
+    end
 
     % Euler-Cauchy Update for states
-    q(i+1) = q(i) + tstep*q_dot(My);
+    q(i+1) = q(i) + tstep*q_dot(My(i));
     theta(i+1) = theta(i) + tstep*theta_dot(q(i));
-    A_xe(i+1) = f_A_xe(Fx, theta(i), u(i));
-    A_ze(i+1) = f_A_ze(Fz, theta(i));
+    A_xe(i+1) = f_A_xe(Fx(i), theta(i), u(i));
+    A_ze(i+1) = f_A_ze(Fz(i), theta(i));
     w(i+1) = w(i) + tstep*w_dot(A_ze(i), q(i), u(i));
-    u(i+1) = u(i) + tstep*u_dot(A_xe(i), q(i), w(i), u(i));
+    u(i+1) = u(i) + tstep*u_dot(A_xe(i), q(i), w(i));
     Xe(i+1) = Xe(i) + tstep*Xe_dot(u(i), w(i), theta(i));
     Ze(i+1) = Ze(i) + tstep*Ze_dot(u(i), w(i), theta(i));
+
+    if mod(i/(length(time)-1)*100, 10) == 0
+        done = i/(length(time)-1)*100;
+        disp([num2str(done),'% done']);
+    end
+
+    if sqrt((Xe(i+1)-Tx)^2 + (Ze(i+1)-Tz)^2) <= 100
+        disp('Hit!');
+        break;
+    elseif sqrt((Xe(i+1)-Tx)^2 + (Ze(i+1)-Tz)^2) >= 1.5*(sqrt((xe0-Tx)^2 + (ze0-Tz)^2))
+        disp('Miss');
+        break;
+    end
+
 end
 
 % Postprocessing
@@ -93,6 +134,7 @@ figure;
 hold on;
 title('xz-plane trajectory');
 scatter(Xe, Ze, 10, 'o', 'filled');
+scatter(Tx, -Tz, 30, 'o', 'filled');
 xlabel('Xe');
 ylabel('Ze');
 grid on;
@@ -172,5 +214,42 @@ xlabel('time');
 ylabel('Xe');
 grid on;
 hold off;
+
+
+figure;
+tiledlayout(3, 1);
+nexttile;
+hold on;
+title('Fx');
+scatter(time(1:end-1), Fx, 3, 'o', 'filled');
+xlabel('time');
+ylabel('Fx');
+grid on;
+hold off;
+
+nexttile;
+hold on;
+title('Fz');
+scatter(time(1:end-1), Fz, 3, 'o', 'filled');
+xlabel('time');
+ylabel('Fz');
+grid on;
+hold off;
+
+nexttile;
+hold on;
+title('My');
+scatter(time(1:end-1), My, 3, 'o', 'filled');
+xlabel('time');
+ylabel('My');
+grid on;
+hold off;
+
+
+
+
+
+
+
 
 
---
+++ OCP_3DOF.m
@@ -0,0 +1,137 @@
+function [control_fz, control_my, flag] = OCP_3DOF(mass, inertia, X, target_position)
+
+opti = casadi.Opti();       % Opti Stack object
+
+% parameters
+N = 20;                     % number of control intervals for shooting
+m = mass;
+Iyy = inertia;
+
+
+% decision variables
+% states: 
+    U = opti.variable(1,N+1);       % x-component of velocity
+    W = opti.variable(1,N+1);       % z-component of velocity
+    Axe = opti.variable(1,N+1);     % acceleration in x (earth-fixed)
+    Aze = opti.variable(1,N+1);     % acceleration in z (earth-fixed)
+    Xe = opti.variable(1,N+1);      % earth-fixed x
+    Ze = opti.variable(1,N+1);      % earth-fixed z
+    Theta = opti.variable(1,N+1);   % pitch angle
+    Q = opti.variable(1,N+1);       % pitch rate
+    T = opti.variable();            % time
+% control input:
+    % Fx = opti.variable(1,N);        % force in x
+    Fz = opti.variable(1,N);        % force in z
+    My = opti.variable(1,N);        % moment around y
+
+
+% objective function
+
+opti.minimize(T);
+
+
+% dynamic constraints
+% define helper variables for CasADi Functions
+    dt = T/N;
+    u = casadi.MX.sym('u');
+    w = casadi.MX.sym('w');
+    axe = casadi.MX.sym('axe');
+    aze = casadi.MX.sym('aze');
+    theta = casadi.MX.sym('theta');
+    q = casadi.MX.sym('q');
+
+    fx = casadi.MX.sym('fx');
+    fz = casadi.MX.sym('fz');
+    my = casadi.MX.sym('my');
+
+% definition of state updates (Euler-Cauchy)
+    u_dot = casadi.Function('u_dot', {axe, q, w}, {axe - q*w}, {'axe' 'q' 'w'}, {'u_dot'});
+    U_next = U(1:N) + dt*u_dot(Axe(1:N),Q(1:N),W(1:N));
+    opti.subject_to(U(2:N+1) == U_next);
+    
+    w_dot = casadi.Function('w_dot', {aze, q, u}, {aze + q*u}, {'aze' 'q' 'u'}, {'w_dot'});
+    W_next = W(1:N) + dt*w_dot(Aze(1:N),Q(1:N),U(1:N));
+    opti.subject_to(W(2:N+1) == W_next);
+
+    % f_axe = casadi.Function('f_Axe', {fx, theta}, {fx/200 - 9.81*sin(theta)}, {'fx' 'theta'}, {'f_axe'});
+    f_axe = casadi.Function('f_Axe', {theta, u}, {50000/200 - 9.81*sin(theta) - 0.05*0.7*1.225*u^2*0.5/200}, {'theta', 'u'}, {'f_axe'});
+    Axe_next = f_axe(Theta(1:N), U(1:N));
+    opti.subject_to(Axe(2:N+1) == Axe_next);
+
+    f_aze = casadi.Function('f_Aze', {fz, theta}, {fz/200 + 9.81*cos(theta)}, {'fz' 'theta'}, {'f_aze'});
+    Aze_next = f_aze(Fz,Theta(1:N));
+    opti.subject_to(Aze(2:N+1) == Aze_next);
+
+    xe_dot = casadi.Function('xe_dot', {u, w, theta}, {u*cos(theta) + w*sin(theta)}, {'u' 'w' 'theta'}, {'xe_dot'});
+    Xe_next = Xe(1:N) + dt*xe_dot(U(1:N),W(1:N),Theta(1:N));
+    opti.subject_to(Xe(2:N+1) == Xe_next);
+
+    ze_dot =casadi.Function('ze_dot', {u, w, theta}, {-u*sin(theta) + w*cos(theta)}, {'u' 'w' 'theta'}, {'ze_dot'});
+    Ze_next = Ze(1:N) + dt*ze_dot(U(1:N),W(1:N),Theta(1:N));
+    opti.subject_to(Ze(2:N+1) == Ze_next);
+
+    q_dot = casadi.Function('q_dot', {my}, {my/1000}, {'my'}, {'q_dot'});
+    Q_next = Q(1:N) + dt*q_dot(My);
+    opti.subject_to(Q(2:N+1) == Q_next);
+    
+    theta_dot = casadi.Function('theta_dot', {q}, {q}, {'q'}, {'theta_dot'});
+    Theta_next = Theta(1:N) + dt*theta_dot(Q(1:N));
+    opti.subject_to(Theta(2:N+1) == Theta_next);
+
+
+% initial states
+opti.subject_to(U(1)==X(1));
+opti.subject_to(W(1)==X(2));
+opti.subject_to(Axe(1)==X(3));
+opti.subject_to(Aze(1)==X(4));
+opti.subject_to(Xe(1)==X(5));
+opti.subject_to(Ze(1)==X(6));
+opti.subject_to(Q(1)==X(7));
+opti.subject_to(Theta(1)==X(8));
+
+% input constraints
+% opti.subject_to(Fx<100000);
+% opti.subject_to(Fx>0);
+opti.subject_to(Fz<100000); 
+opti.subject_to(Fz>-100000);
+opti.subject_to(My<10000); 
+opti.subject_to(My>-10000);
+
+% state constraints
+
+
+% time constraint
+opti.subject_to(T>=0);
+
+% distance to target within delta
+delta = 100;
+opti.subject_to(  sqrt((Xe(N+1)-target_position(1))^2 + (Ze(N+1)-target_position(2))^2)  <= delta   );
+
+
+% solve the problem
+solver_options = struct;
+solver_options.ipopt.print_level = 0;
+solver_options.print_time = 0;
+solver_options.verbose = 0;
+solver_options.ipopt.sb ='yes';
+
+opti.solver('ipopt', solver_options);           % select solver
+
+try
+    sol = opti.solve();
+    % Fx_traj = sol.value(Fx);
+    Fz_traj = sol.value(Fz);
+    My_traj = sol.value(My);
+    control_fx = Fx_traj(1);
+    control_fz = Fz_traj(1);
+    control_my = My_traj(1);
+    flag = 0;
+catch
+    % control_fx = nan;
+    control_fz = nan;
+    control_my = nan;
+    flag = 1;
+end
+
+end
+
